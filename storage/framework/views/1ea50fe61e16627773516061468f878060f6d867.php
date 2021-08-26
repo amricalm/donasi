f429a941777059c6cd29a8da69eb56adab95e5a0
amricalm <amri.m99@gmail.com>
2021-08-26 15:10:30 +0700

    update form login versi mobile

--- storage/framework/views/1ea50fe61e16627773516061468f878060f6d867.php
+++ storage/framework/views/1ea50fe61e16627773516061468f878060f6d867.php
@@ -1,112 +1,117 @@
-<!DOCTYPE html>
-<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<!doctype html>
+<html lang="en" class="h-100">
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
-    <title>Qoryah Qur'an - Admin Login</title>
-    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <link rel="icon" href="<?php echo e(asset('img/favicon.ico')); ?>" type="image/x-icon">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('files/bower_components/bootstrap/css/bootstrap.min.css')); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('files/assets/pages/waves/css/waves.min.css')); ?>" media="all">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('files/assets/icon/feather/css/feather.css')); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('files/assets/icon/themify-icons/themify-icons.css')); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('files/assets/icon/icofont/css/icofont.css')); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('files/assets/icon/font-awesome/css/font-awesome.min.css')); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('files/assets/css/style.css')); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('files/assets/css/pages.css')); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/adn.css')); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('files/bower_components/sweetalert2/sweetalert2.css')); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="generator" content="">
+    <title>Si Kesjaor - Mobile</title>
+    <!-- manifest meta -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="<?php echo e(asset('vendor/finwallapp/img/favicon180.png')); ?>" sizes="180x180">
+    <link rel="icon" href="<?php echo e(asset('vendor/finwallapp/img/favicon32.png')); ?>" sizes="32x32" type="image/png">
+    <link rel="icon" href="<?php echo e(asset('vendor/finwallapp/img/favicon16.png')); ?>" sizes="16x16" type="image/png">
+    <!-- Material icons-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+    <!-- swiper CSS -->
+    <link href="<?php echo e(asset('vendor/finwallapp/vendor/swiper/css/swiper.min.css')); ?>" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="<?php echo e(asset('vendor/finwallapp/css/style.css')); ?>" rel="stylesheet" id="style">
 </head>
 
-<body>
-    <div id="app" themebg-pattern="theme5">
-        <header id="header" class="bg-header">
-            <nav class="flex items-center px-2 py-2 text-center">
-                <a href="/" class="mx-auto">
-                    <img class="h-6" src="<?php echo e(url('img/logo.png')); ?>">
-                </a>
-            </nav>
-        </header>
-        <main class="py-4">
-            <section class="login-block">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <form class="md-float-material form-material" method="POST" id="frm" action="<?php echo e(url('login/validasi')); ?>" role="form" onsubmit="return submited()">
-                                <?php echo csrf_field(); ?>
-                                <div class="auth-box card">
-                                    <div class="card-block">
-                                        <div class="text-center pb-4 pt-2">
-                                            ADMIN LOGIN
-                                        </div>
-                                        <div class="form-group form-primary<?php echo e($errors->has('email') ? ' has-error' : ''); ?> form-static-label">
-                                            <input id="email" type="text" name="username" class="form-control" autocomplete="off" required>
-                                            <?php if($errors->has('email')): ?>
-                                            <span class="form-bar">
-                                                <strong><?php echo e($errors->first('email')); ?></strong>
-                                            </span>
-                                            <?php endif; ?>
-                                            <label class="float-label">Username</label>
-                                        </div>
-                                        <div class="form-group form-primary<?php echo e($errors->has('password') ? ' has-error' : ''); ?> form-static-label">
-                                            <input id="password" type="password" name="password" class="form-control<?php echo e($errors->has('password') ? ' is-invalid' : ''); ?>" required="">
-                                            <?php if($errors->has('password')): ?>
-                                            <span class="form-bar">
-                                                <strong><?php echo e($errors->first('password')); ?></strong>
-                                            </span>
-                                            <?php endif; ?>
-                                            <label class="float-label">Password</label>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="row m-t-30">
-                                                <div class="col-md-12">
-                                                    <button type="submit" id="btnSubmit" class="btn btn-primary btn-md btn-block text-center m-b-20" data-loading-text="Sedang Proses...">Login</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                            </form>
-                        </div>
+<body class="body-scroll d-flex flex-column h-100 menu-overlay">
+    <!-- screen loader -->
+    <div class="container-fluid h-100 loader-display">
+        <div class="row h-100">
+            <div class="align-self-center col">
+                <div class="logo-loading">
+                    <div class="icon icon-100 mb-4 rounded-circle">
+                        <img src="<?php echo e(asset('vendor/finwallapp/img/favicon144.png')); ?>" alt="" class="w-100">
+                    </div>
+                    <h4 class="text-default">SI Kesjaor</h4>
+                    <p class="text-secondary">Mobile</p>
+                    <div class="loader-ellipsis">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
                     </div>
                 </div>
-            </section>
-        </main>
+            </div>
+        </div>
     </div>
-    <script type="text/javascript" src="<?php echo e(asset('files/bower_components/jquery/js/jquery.min.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/bower_components/jquery-ui/js/jquery-ui.min.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/bower_components/popper.js/js/popper.min.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/bower_components/bootstrap/js/bootstrap.min.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/assets/pages/waves/js/waves.min.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/bower_components/jquery-slimscroll/js/jquery.slimscroll.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/bower_components/modernizr/js/modernizr.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/bower_components/modernizr/js/css-scrollbars.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/bower_components/i18next/js/i18next.min.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/bower_components/i18next-xhr-backend/js/i18nextXHRBackend.min.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/bower_components/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/bower_components/jquery-i18next/js/jquery-i18next.min.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('files/assets/js/common-pages.js')); ?>"></script>
+    <form class="md-float-material form-material" method="POST" id="frm" action="<?php echo e(url('login/validasi')); ?>" role="form" onsubmit="return submited()">
+        <?php echo csrf_field(); ?>
+        <!-- Begin page content -->
+        <main class="flex-shrink-0 main has-footer">
+            <!-- Fixed navbar -->
+            <header class="header">
+                <div class="row">
+                    <div class="col-auto px-0">
+                        <button class="btn btn-40 btn-link back-btn" type="button">
+                            <span class="material-icons">keyboard_arrow_left</span>
+                        </button>
+                    </div>
+                    <div class="text-left col align-self-center">
+
+                    </div>
+                    <div class="ml-auto col-auto align-self-center">
+                        <a href="#" class="text-white">
+                            Sign up
+                        </a>
+                    </div>
+                </div>
+            </header>
+            <div class="container h-100 text-white">
+                <div class="row h-100">
+                    <div class="col-12 align-self-center mb-4">
+                        <div class="row justify-content-center">
+                            <div class="col-11 col-sm-7 col-md-6 col-lg-5 col-xl-4">
+                                <h2 class="font-weight-normal mb-5">Si Kesjaor</h2>
+                                <div class="form-group">
+                                    <label class="text-white active">Username</label>
+                                    <input type="text" class="form-control" name="username" id="email">
+                                </div>
+                                <div class="form-group">
+                                    <label class="text-white position-relative">Password</label>
+                                    <input type="password" class="form-control" name="password" id="password">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </main>
+        <!-- footer-->
+        <div class="footer no-bg-shadow py-3">
+            <div class="row justify-content-center">
+                <div class="col">
+                    <button type="submit" id="btnSubmit" class="btn btn-default rounded btn-block" data-loading-text="Sedang Proses...">Login</button>
+                </div>
+            </div>
+        </div>
+    </form>
+    <!-- Required jquery and libraries -->
+    <script src="<?php echo e(asset('vendor/finwallapp/js/jquery-3.3.1.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('vendor/finwallapp/js/popper.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('vendor/finwallapp/vendor/bootstrap/js/bootstrap.min.js')); ?>"></script>
+    <!-- cookie js -->
+    <script src="<?php echo e(asset('vendor/finwallapp/js/jquery.cookie.js')); ?>"></script>
+    <!-- Swiper slider  js-->
+    <script src="<?php echo e(asset('vendor/finwallapp/vendor/swiper/js/swiper.min.js')); ?>"></script>
+    <!-- Customized jquery file  -->
+    <script src="<?php echo e(asset('vendor/finwallapp/js/main.js')); ?>"></script>
+    <script src="<?php echo e(asset('vendor/finwallapp/js/color-scheme-demo.js')); ?>"></script>
+    <!-- page level custom script -->
+    <script src="<?php echo e(asset('vendor/finwallapp/js/app.js')); ?>"></script>
     <script type="text/javascript" src="<?php echo e(asset('files/bower_components/sweetalert2/sweetalert2.all.js')); ?>"></script>
     <script type="text/javascript">
-        var msg = '<?php echo e(Session::get('
-        alert ')); ?>';
-        var exist = '<?php echo e(Session::has('
-        alert ')); ?>';
-        if (exist) {
-            Swal.fire({
-                type: 'warning',
-                title: 'Oops...',
-                text: msg
-            })
-        }
-        $(document).ready(function() {
-            $('#email').focus();
-        });
-
         function submited() {
             $('#btnSubmit').removeClass('btn-primary');
             $('#btnSubmit').addClass('btn-warning');
